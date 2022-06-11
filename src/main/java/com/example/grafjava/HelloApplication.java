236488f3a6fd68707d18e9e2f1b2792c30f8c2c9
Szade <wojciech.szade@gmail.com>
2022-06-11 18:31:57 +0200

    change pom.xml configuration to run with mvn

--- src/main/java/com/example/grafjava/HelloApplication.java
+++ src/main/java/com/example/grafjava/HelloApplication.java
@@ -1,11 +1,15 @@
 package com.example.grafjava;
 
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Stack;
+
+
 
 public class HelloApplication extends Application {
     @Override
@@ -15,6 +19,8 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+    
+
     }
 
     public static void main(String[] args) {
