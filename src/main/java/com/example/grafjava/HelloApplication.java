db57293d9bcba3233ded242778e2388f44d36cf2
Szade <wojciech.szade@gmail.com>
2022-06-11 18:38:49 +0200

    add main function to HelloApplication
    the main function starts after closing the gui

--- src/main/java/com/example/grafjava/HelloApplication.java
+++ src/main/java/com/example/grafjava/HelloApplication.java
@@ -1,5 +1,6 @@
 package com.example.grafjava;
 
+import kod.*;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,7 +8,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Stack;
 
 
 
@@ -19,11 +19,37 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-    
+        
 
     }
 
     public static void main(String[] args) {
         launch();
+        Labirynt l = new Labirynt(10, 10);
+
+        if( args.length == 1 ) l.genWagi();
+
+        if (args.length == 2) {
+            try {
+                l = Utils.readLabirynt(args[1]);
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+                return;
+            }
+        }
+        System.out.println("wczytano " + l.getN());
+        System.out.println(l.testWag());
+
+        Dijkstra d = new Dijkstra(l, 0);
+        d.start();
+        try {
+            d.join();
+        } catch (InterruptedException e) {
+            System.err.println(e.getLocalizedMessage());
+        }
+        l.dziel(5);
+        Bfs bfs = new Bfs(l, 0);
+        bfs.start();
+        System.out.println("stan spojnosci grafu:" + bfs.getSpojny());
     }
 }
