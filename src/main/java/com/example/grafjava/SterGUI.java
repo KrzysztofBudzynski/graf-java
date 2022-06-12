31bb8dbe723ebb22cbc0060b557d3dbee2508741
Szade <wojciech.szade@gmail.com>
2022-06-12 02:44:38 +0200

    change buttons syntax
    height and width working, fileSelect in progress

--- src/main/java/com/example/grafjava/SterGUI.java
+++ src/main/java/com/example/grafjava/SterGUI.java
@@ -1,16 +1,22 @@
 package com.example.grafjava;
 
+import java.io.File;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.*;
+
 import kod.Bfs;
 import kod.Dijkstra;
 import kod.Labirynt;
 import kod.Utils;
+import com.example.grafjava.HelloApplication;
 
 public class SterGUI {
-    public TextField wymiary;
-    public TextField nazwa;
+    public TextField szerokosc;
+    public TextField wysokosc;
+    public String nazwa;
     private Labirynt l = null;
     private Dijkstra d;
     private Bfs b;
@@ -45,34 +51,72 @@ public class SterGUI {
     private void genClicked() {
         int width;
         int height;
-        String [] words = wymiary.getText().split("\\s+");
+        String slowoWys;
+        String slowoSzer;
+        int maxWymiar = 10000;
+        //String [] words = wymiary.getText().split("\\s+");
 
-        if( words.length != 2 ) {
-            System.err.println("Niepoprawne dane w okno nazwa");
+        // if( words.length != 2 ) {
+        //     System.err.println("Niepoprawne dane w okno nazwa");
+        //     return;
+        // }
+
+        // height = Integer.parseInt(words[0]);
+        // width = Integer.parseInt(words[1]);
+
+        slowoWys = wysokosc.getText();
+        height = Integer.parseInt(slowoWys);
+        if (height > maxWymiar){
+            System.err.println("Przekroczono maksymalny wymiar dla wysokosci.");
+            return;
+        }
+        else if (height <= 0){
+            System.err.println("Podano wymiar wysokosci nizszy niz 0.");
+            return;
+        }
+        slowoSzer = szerokosc.getText();
+        width = Integer.parseInt(slowoSzer);
+        if (width > maxWymiar){
+            System.err.println("Przekroczono maksymalny wymiar szerokosci.");
+            return;
+        }
+        else if (width <= 0){
+            System.err.println("Podano wymiar szerokosci nizszy niz 0.");
             return;
         }
 
-        height = Integer.parseInt(words[0]);
-        width = Integer.parseInt(words[1]);
-
-        if( width <= 0 || height <= 0 ) {
-            System.err.println("Podano rozmiary mniejsze od 1");
-            return;
-        }
+        // if( width <= 0 || height <= 0 ) {
+        //     System.err.println("Podano rozmiary mniejsze od 1");
+        //     return;
+        // }
         l = new Labirynt(height, width);
         l.genWagi();
-        System.out.println("Wygenerowano");
+        System.out.println("Wygenerowano, wysokosc " + height + " szerokosc " + width + "\n");
     }
+    @FXML
+    private void fileSelect() {
+        System.out.println("fileSelect\n");
+    }
+    //     FileChooser filechooser = new FileChooser();
+    //     Stage stage = (Stage) fileSelect.getScene(),getWindow();
+    //     File file = filechooser.showOpenDialog();
+    //     if (file != null)
+    //     {
+    //         nazwa = file.getAbsolutePath();
+    //     }
+    //     System.out.println(nazwa);
+    // }
+    
 
     @FXML
     private void inpClicked() {
         try {
-            l = Utils.readLabirynt(nazwa.getText());
+           l = Utils.readLabirynt(nazwa);
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
         if( l != null ) {
-            System.out.println("Wczytano z: " + nazwa.getText());
+            System.out.println("Wczytano z: " + nazwa);
         }
     }
 }
